424a225fdcfd27b4c19d82245362f9d25d60382d
Ajay Vallabh <ajay.vallabh@mail.com>
2013-11-16 14:48:36 -0500

    Check bounds, keep score

--- ball.rb
+++ ball.rb
@@ -1,12 +1,43 @@
 class Ball
-
-  SIZE = 8
-
-  attr_reader :x, :y
+  SIZE = 16
+  attr_reader :x, :y, :angle, :speed
 
   def initialize
     @x = Pong::WIDTH/2
     @y = Pong::HEIGHT/2
+    @angle = rand(120) + 30
+    @angle *= -1 if rand > 0.5
+    @speed = 4
+  end
+
+  def dx; Gosu.offset_x(angle, speed); end
+  def dy; Gosu.offset_y(angle, speed); end
+
+  def move!
+    @x += dx
+    @y += dy
+
+    if @y < 0
+      @y = 0
+      bounce_off_edge!
+    end
+    if @y > Pong::HEIGHT
+      @y = Pong::HEIGHT
+      bounce_off_edge!
+    end
+
+    if @x < 0
+      @x = 0
+      @angle = Gosu.angle(0, 0, -dx, dy)
+    end
+    if @x > Pong::WIDTH
+      @x = Pong::WIDTH
+      @angle = Gosu.angle(0, 0, -dx, dy)
+    end
+  end
+
+  def bounce_off_edge!
+    @angle = Gosu.angle(0, 0, dx, -dy)
   end
 
   def x1; @x - SIZE/2; end
@@ -23,5 +54,12 @@ class Ball
       x2, y2, color,
       x2, y1, color,
       )
+  end
+
+  def off_left?
+    x1 < 0
+  end
+  def off_right?
+    x2 > Pong::WIDTH
   end
 end
